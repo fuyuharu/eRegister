884264ab521d92fabc2b934982e528c9bd995c11
marcel <greskovics.marcell@gmail.com>
2017-12-14 20:24:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/marcel/eRegister/LoginActivity.java
@@ -0,0 +1,78 @@
+package com.example.marcel.eRegister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button buttonLogin;
+    EditText loginUsername, loginPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        loginUsername = (EditText) findViewById(R.id.loginUsername);
+        loginPassword = (EditText) findViewById(R.id.loginPassword);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+
+    private void Login() {
+
+        String url = "https://deak.enaplo.net/ajax/login/login.php";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.trim().equals("statusmsg('<b>Hibás felhasználónév / jelszó</b>');")) {
+                    Toast.makeText(LoginActivity.this, "Post Request Success", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Post Request Faliure", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, "error:" + error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("uname", loginUsername.getText().toString().trim());
+                params.put("pw", loginPassword.getText().toString().trim());
+                return super.getParams();
+            }
+        };
+        requestQueue.add(stringRequest);
+
+
+    }
+}
