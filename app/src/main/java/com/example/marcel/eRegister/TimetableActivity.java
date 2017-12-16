7f232899c04e62be3bb707716f08c94e0676585c
marcel <greskovics.marcell@gmail.com>
2017-12-16 10:58:06 +0100

    Added day selector spinner to UserArea

--- app/src/main/java/com/example/marcel/eRegister/TimetableActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.marcel.eRegister;
-
-/**
- * Created by marcel on 12/15/2017.
- */
-
-public class TimetableActivity {
-}
--- app/src/main/java/com/example/marcel/eRegister/UserAreaActivity.java
+++ app/src/main/java/com/example/marcel/eRegister/UserAreaActivity.java
@@ -2,19 +2,25 @@ package com.example.marcel.eRegister;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class UserAreaActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     ActionBar actionBar;
+    private Spinner spinner;
+    private static final String[]paths = {"item 1", "item 2", "item 3"};
 
 
 
@@ -22,6 +28,8 @@ public class UserAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+
+
 
             Window window = this.getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -47,4 +55,20 @@ public class UserAreaActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_spinner, menu);
+
+        MenuItem item = menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_list_item_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        return true;
+    }
+
 }
