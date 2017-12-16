daeffc6cb161aa229d19b2c465cec4cc5ccae7cf
DESKTOP-SV1MC5G\Tom Quinders <tomquinders@gmail.com>
2017-12-16 20:21:12 +0100

    Created drawerSetup() which makes the drawer items selectable

--- app/src/main/java/com/example/marcel/eRegister/UserAreaActivity.java
+++ app/src/main/java/com/example/marcel/eRegister/UserAreaActivity.java
@@ -1,7 +1,11 @@
 package com.example.marcel.eRegister;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -25,8 +29,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.marcel.eRegister.fragments.TimetableFragment;
 
-public class UserAreaActivity extends AppCompatActivity {
+
+public class UserAreaActivity extends AppCompatActivity implements TimetableFragment.OnFragmentInteractionListener {
 
     protected FrameLayout frameLayout;
     protected ListView mDrawerList;
@@ -52,18 +58,74 @@ public class UserAreaActivity extends AppCompatActivity {
         actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#eb3349")));
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+        drawerSetUp();
     }
+
+    /**ANNOTATION: This is the view that holds your drawer, the "menu" in that view contains
+     * the drawer items. You set up a new listener for those items being selected and in that
+     * listener, we say what should happen when a certain item was selected.
+     */
+    private void drawerSetUp() {
+        NavigationView mNavigationView = findViewById(R.id.navigation);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fagment;
+                FragmentTransaction transFag = getFragmentManager().beginTransaction();
+
+                switch (item.getItemId()) {
+                    case R.id.nav_grades:
+
+                        break;
+                    case R.id.nav_logout:
+
+                        break;
+                    case R.id.nav_settings:
+
+                        break;
+                    case R.id.nav_timetable:
+
+                        fagment = new TimetableFragment();
+                        transFag.replace(R.id.content_frame,fagment);
+                        transFag.addToBackStack(null);
+                        transFag.commit();
+
+                        //ANNOTATION: This is how you would do it with an activity
+                        //Intent i = new Intent(UserAreaActivity.this, TimetableActivity.class);
+                        //startActivity(i);
+                        break;
+                    default:
+                        break;
+                }
+
+                /**ANNOTATION:
+                 * If you want to clean the code a bit later on, make sure that a fragment is created
+                 * in the switch/case "default" statement, then take the code that is
+                 *
+                 transFag.replace(R.id.content_frame,fagment);
+                 transFag.addToBackStack(null);
+                 transFag.commit();
+                 *
+                 * out of the individual case and just work with the variables after the switch/case
+                 * statement. The fragment creation is the only thing you'd need to handle in that.
+                 */
+                return (true);
+            }
+        });
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -82,4 +144,8 @@ public class UserAreaActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //ANNOTATION: Here you activity can communicate with the fragment... I guess
+    }
 }
